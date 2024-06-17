596bb3d0f111191d1a07e857059a7aa2ca531299
Rubi <rubi.cloud87@gmail.com>
2024-06-17 18:06:34 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,84 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+##
+
+resource "aws_vpc" "nestjs_vpc" {
+  cidr_block = var.CIDR
+  tags = {
+    name = "vpc-nestjs"
+  }
+}
+
+resource "aws_subnet" "public_subnet1" {
+  vpc_id            = aws_vpc.nestjs_vpc.id
+  cidr_block        = var.CIDR-PUBLIC
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_internet_gateway" "nestjs_igw" {
+  vpc_id = aws_vpc.nestjs_vpc.id
+}
+
+
+
+resource "aws_route_table" "public_subnet1_rt" {
+  vpc_id = aws_vpc.nestjs_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.nestjs_igw.id
+  }
+}
+
+
+resource "aws_route_table_association" "public_subnet1_rt" {
+  subnet_id      = aws_subnet.public_subnet1.id
+  route_table_id = aws_route_table.public_subnet1_rt.id
+}
+
+resource "aws_security_group" "nestjs_sg" {
+  name        = "NestJS_SG"
+  vpc_id      = aws_vpc.nestjs_vpc.id
+  description = "Security Group for NestJS Server"
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = "3000"
+    to_port     = "3000"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "NestJS-SG"
+  }
+}
+
+resource "aws_instance" "nestjs" {
+  ami                         = "ami-06784e9934d6ab0f5"
+  instance_type               = "t2.small"
+  vpc_security_group_ids         = [aws_security_group.nestjs_sg.id]
+  associate_public_ip_address = true
+  subnet_id = aws_subnet.public_subnet1.id
+}
